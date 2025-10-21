0fe3a9e00b18c52eb159e18b35522d56e5883966
Anita David <anitafunmilayo@gmail.com>
2025-10-21 19:48:07 +0100

    created simple user interface with interactivity

---
+++ src/data/card.js
@@ -0,0 +1,92 @@
+export const naijaCards = [
+  {
+    word: "Jollof Rice",
+    description: ["spicy", "tomato", "party food", "West-African pride"],
+  },
+  { word: "Suya", description: ["grilled", "beef", "street", "spicy"] },
+  {
+    word: "Afrobeats",
+    description: ["vibrant", "rhythmic", "global", "danceable"],
+  },
+  { word: "Puff-Puff", description: ["fried", "sweet", "snack", "fluffy"] },
+  {
+    word: "Aso Ebi",
+    description: ["uniform", "colorful", "wedding", "family"],
+  },
+  { word: "Zobo", description: ["hibiscus", "drink", "sweet", "refreshing"] },
+  {
+    word: "Egusi Soup",
+    description: ["melon", "thick", "savory", "Nigerian classic"],
+  },
+  { word: "Gele", description: ["headwrap", "elegant", "bold", "traditional"] },
+
+  {
+    word: "Palm Wine",
+    description: ["fermented", "natural", "sweet", "rural"],
+  },
+  { word: "Ofada Rice", description: ["local", "fragrant", "sauce", "spicy"] },
+
+  {
+    word: "Keke Napep",
+    description: ["tricycle", "yellow", "bumpy", "transport"],
+  },
+  { word: "Aso Rock", description: ["Abuja", "political", "hill", "power"] },
+  { word: "Ojuelegba", description: ["busy", "Lagos", "hustle", "traffic"] },
+  { word: "Nollywood", description: ["films", "drama", "industry", "nigerian film"] },
+  { word: "Chin Chin", description: ["crunchy", "snack", "sweet", "golden"] },
+  { word: "Okada", description: ["motorcycle", "fast", "risky", "transport"] },
+  {
+    word: "Boli",
+    description: ["roasted", "plantain", "groundnut", "golden-yellow"],
+  },
+  {
+    word: "Wakanda Spirit",
+    description: ["African", "pride", "resilience", "movie"],
+  },
+  {
+    word: "Akara",
+    description: ["bean cake", "fried", "morning snack", "crispy"],
+  },
+  { word: "Amala", description: ["yam flour", "dark", "ibadan", "swallow"] },
+  { word: "Dodo", description: ["fried", "plantain", "sweet", "golden"] },
+
+  { word: "Owambé", description: ["party", "lace", "music", "food galore"] },
+
+  { word: "Danfo", description: ["bus", "yellow", "chaotic", "Lagos traffic"] },
+  { word: "Okirika", description: ["thrift", "market", "cheap", "imported"] },
+  { word: "Japa", description: ["escape", "abroad", "trend", "migration"] },
+  { word: "Sapa", description: ["broke", "struggle", "relatable", "poor"] },
+  {
+    word: "Asake",
+    description: ["fuji fusion", "street", "melodic", "hitmaker"],
+  },
+
+  {
+    word: "Twitter ",
+    description: ["chaotic", "funny", "trending", "drag culture"],
+  },
+  {
+    word: "Nepa",
+    description: ["electricity", "power outage", "shout", "nationwide"],
+  },
+
+  {
+    word: "Ginger",
+    description: ["motivation", "hype", "energy", "Naija word"],
+  }, {
+    word: "Fela Kuti",
+    description: ["legend", "sax", "protest", "afrobeat	"],
+  },
+	 {
+    word: "Gen Z",
+    description: ["witty", "bold", "online", "fearless"],
+  },
+	 {
+    word: "Nigerian Mother",
+    description: ["strict", "loving", "dramatic", "legendary"],
+  },
+	 {
+    word: "Morning Devotion",
+    description: ["early", "prayer", "chorus", "routine"],
+  },
+];
